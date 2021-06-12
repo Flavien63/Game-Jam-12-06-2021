6a1c069bda018938ac83eae9cdf1b5b17a2ded2c
Rouxeyz <85785335+Rouxeyz@users.noreply.github.com>
2021-06-12 18:05:41 +0200

    Add files via upload

--- perso.h
+++ perso.h
@@ -6,8 +6,8 @@ typedef struct etat_perso
     int x;
     int y;
     int saut;
-    int mort;  //0 dans le niveau vie, 1 dans le niveau mort
-}etat_perso_t;
+    int mort; //0 dans le niveau vie, 1 dans le niveau mort
+} etat_perso_t;
 
 void init_grille();
 void affichage();
@@ -18,21 +18,17 @@ int rien_cote(int droite);
 int rien_droite();
 int rien_gauche();
 
-
 int decalage_perso_haut();
 int decalage_perso_droite();
 int decalage_perso_gauche();
-
-
 
 int touche_sol();
 int deplacement_perso(int vertical, int horizontal);
 int gravite();
 
 void effacement_perso();
-void placement_perso();
+int placement_perso();
 
-
-
+int case_change_etat(int valeur_case);
 
 #endif
