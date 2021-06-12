7afd6d2dd55268a55bf9bb7be6a4839cf43b968d
ramalac <85785387+ramalac@users.noreply.github.com>
2021-06-12 13:25:31 +0200

    Update dessiner.h

--- dessiner.h
+++ dessiner.h
@@ -1,5 +1,8 @@
 #ifndef dessiner_h
 #define dessiner_h
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
 
 void dessingrille(int grille[n][m], SDL_Renderer *renderer, couleurs[nb][3]);
 
