c8b30e1b608516a0181e5e057f33b41e4d79a280
herrlockskun <85789614+herrlockskun@users.noreply.github.com>
2021-06-12 16:55:27 +0200

    Update couleurs.c

--- couleurs.c
+++ couleurs.c
@@ -3,12 +3,22 @@
 void  tableau(int tab[100][3])
 {
 	/*couleur du ciel */
-	tab[0][0]=51;
-	tab[0][1]=102;
-	tab[0][2]=255; 
+	tab[0][0]=51; tab[0][1]=102; tab[0][2]=255; 
+	
 	/*couleur du sol*/
-	tab[1][0]=153;
-	tab[1][1]=77;
-	tab[1][2]=0;
+	/*_marron_*/
+	tab[1][0]=153;tab[1][1]=77;tab[1][2]=0;
+	/*_vertforet_*/
+	tab[2][0]=153;tab[2][1]=77;tab[2][2]=0;
+	/*_sable_*/
+	tab[3][0]=153;tab[3][1]=77;tab[3][2]=0;
+	
+	/*obstacle*/
+	/*_pointe-acier_*/
+	tab[20][0]=143;tab[20][1]=143;tab[20][2]=143;
+	/*_pointe-bois_*/
+	tab[21][0]=119;tab[21][1]=70;tab[21][2]=52;
+	
+	
 }
 
