60f011b69c986b74cda685d99d6a2cbe6300b576
herrlockskun <85789614+herrlockskun@users.noreply.github.com>
2021-06-12 13:18:21 +0200

    Add files via upload

---
+++ couleurs.c
@@ -0,0 +1,12 @@
+#include "couleurs.h"
+
+int * tableau()
+{
+	int tab[100][3];
+	tab[0][0]=51;
+	tab[0][1]=102;
+	tab[0][2]=255;
+
+	return tab;
+}
+
---
+++ couleurs.h
@@ -0,0 +1,6 @@
+#ifndef couleurs_h
+#define couleurs_h
+
+int * tableau();
+
+#endif
