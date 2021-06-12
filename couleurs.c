eb5b086199224f397f08a653cfe1e38cb88062d2
herrlockskun <85789614+herrlockskun@users.noreply.github.com>
2021-06-12 13:39:34 +0200

    Update couleurs.c

--- couleurs.c
+++ couleurs.c
@@ -3,9 +3,15 @@
 int * tableau()
 {
 	int tab[100][3];
+	/*couleur du ciel */
 	tab[0][0]=51;
 	tab[0][1]=102;
-	tab[0][2]=255;
+	tab[0][2]=255; 
+	/*couleur du sol*/
+	tab[1][0]=153;
+	tab[1][1]=77;
+	tab[1][2]=0;
+	
 
 	return tab;
 }
