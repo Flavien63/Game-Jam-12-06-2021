00c033167ef9676efd95b037dc4426161541703d
herrlockskun <85789614+herrlockskun@users.noreply.github.com>
2021-06-12 13:33:01 +0200

    Update main.h

--- main.h
+++ main.h
@@ -1,5 +1,7 @@
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_ttf.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include "dessiner.h"
+#include "couleurs.h"
 
 int main();
