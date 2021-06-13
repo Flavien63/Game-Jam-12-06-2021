7c1a8944df9187844eb4af8d808d16e1ecb45293
ramalac <85785387+ramalac@users.noreply.github.com>
2021-06-13 10:26:17 +0200

    Update init_level.c

--- init_level.c
+++ init_level.c
@@ -1,13 +1,17 @@
 #include "init_level.h"
 
-void import_level(int mat[200][300], FILE * level_file)
+void import_level(int mat[200][300], FILE * level_file,int chargerpos,etat_perso_t *etat)
 {
     int pos_x, pos_y;
     int i = 0, j = 0;
 
     fscanf(level_file, "%d", &pos_x);
     fscanf(level_file, "%d", &pos_y);
-    
+    if (chargerpos)
+    {
+        etat->x=pos_x;
+        etat->y=pos_y;
+    }
     for (i=0; i < 200; i++)
     {
         for (j=0;j < 300; j++)
@@ -17,12 +21,12 @@ void import_level(int mat[200][300], FILE * level_file)
     }
 }
 
-void import_file(int mat[200][300], const char * level_file)
+void import_file(int mat[200][300], const char * level_file, int chargerpos, etat_perso_t* etat)
 {
     FILE * file = fopen(level_file, "r");
     if (file)
     {
-        import_level(mat, file);
+        import_level(mat, file, chargerpos,etat);
     }
 
     fclose(file);
