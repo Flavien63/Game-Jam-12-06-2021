d0536efe43340dff26404476d9cd5453d8d321e6
ramalac <85785387+ramalac@users.noreply.github.com>
2021-06-13 10:26:38 +0200

    Update init_level.h

--- init_level.h
+++ init_level.h
@@ -3,9 +3,10 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include "perso.h"
 
-void import_level(int mat[200][300], FILE * level_file);
+void import_level(int mat[200][300], FILE * level_file,int chargerpos,etat_perso_t *etat);
 
-void import_file(int mat[200][300], const char * level_file);
+void import_file(int mat[200][300], const char * level_file,int chargerpos,etat_perso_t* etat);
 
 #endif
